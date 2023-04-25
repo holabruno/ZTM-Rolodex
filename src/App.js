39fb6e7ff20f7bacf885fc54081085bbaad710ed
Bruno Theoret <holabruno@gmail.com>
2023-04-25 10:13:47 -0400

    first commit - monster rolodex: filter monsters 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super();
 
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        );
+      });
+  }
+
+
+  render() {
+    return (
+      <div className="App" >
+        <h1>Monster Rolodex</h1>
+        <input
+          name="search"
+          type="search"
+          placeholder="search monsters"
+          onChange={(evt) => {
+            console.log({ startingArray: this.state.monsters });
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              const searchString = evt.target.value.toLocaleLowerCase();
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            },
+              () => { console.log({ endingArray: this.state.monsters }) }
+            )
+          }}
+        />
+        {this.state.monsters.map((el) => {
+          return (
+            <>
+              <div key={el.id}>
+                <h2>{el.name}</h2>
+              </div>
+            </>
+          );
+        })
+        }
+      </div >
+    );
+  }
+}
 export default App;
